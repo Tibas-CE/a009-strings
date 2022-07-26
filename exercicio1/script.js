19b7dc0b5ed5eab42170abbc0816d295fd929c42
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-07-25 21:29:26 -0300

    feito

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nomeDoUsuario = prompt("Qual o seu nome?");
+const comida = prompt("Digiti sua comida favorita");
+const comida1 = prompt("Mais uma vez");
+const comida2 = prompt("Prometo que é a última");
+
+console.log(`Estas são as comidas favoritas de ${nomeDoUsuario}: \n-${comida} \n-${comida1} \n-${comida2} \nAgradecemos as suas respostas`);
